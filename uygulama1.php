d402717938724f03430fad05da6f478e1457db7f
sudeunver72 <unversude139@gmail.com>
2026-01-10 18:17:27 +0300

    Add files via upload

---
+++ uygulama1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Degiskenler Ve Sabitler</title>
+</head>
+<body>
+    <?php 
+    $sayi1=10; #sayi1 degiskenine 10 degerini atadim.
+    $sayi2=5;  #sayi2 degiskenine 5 degerini atadim.
+    echo $sayi1+$sayi2; #sayi1 ve sayi2 degiskenlerinin icindeki degerleri topladim ve echo komutuyla yazdirdim.
+    echo "<br\>";#bu komutla alt satira gecmesini sagladim.
+    echo $sayi1-$sayi2;#sayi1 ve sayi2 degiskenlerinin icindeki degerlerin farkini aldim  ve echo komutuyla yazdirdim.
+    echo "<br\>";#bu komutla alt satira gecmesini sagladim.
+    echo $sayi1*$sayi2;#sayi1 ve sayi2 degiskenlerinin icindeki degerleri carptim  ve echo komutuyla yazdirdim.
+
+    ?>
+
+    
+</body>
+</html>
